176a6c662d38ac0ca40b8ef30f53cf58990398fe
John  Owusu Duah <johnowusuduah@Johns-MBP.lan>
2021-09-16 17:03:07 -0400

    Plot Labelled

--- script.py
+++ script.py
@@ -1,14 +1,27 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-data = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
+data = pd.read_csv(
+    "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
 data.head()
 
-data_new = data.loc[:,["Mortality rate, infant (per 1,000 live births)", "GDP per capita (constant 2010 US$)","Country Name"]]
+data_new = data.loc[
+    :,
+    [
+        "Mortality rate, infant (per 1,000 live births)",
+        "GDP per capita (constant 2010 US$)",
+        "Country Name",
+    ],
+]
 
 
-
-plt.scatter(data = data_new, x="GDP per capita (constant 2010 US$)", y="Mortality rate, infant (per 1,000 live births)")
+plt.scatter(
+    data=data_new,
+    x="GDP per capita (constant 2010 US$)",
+    y="Mortality rate, infant (per 1,000 live births)",
+)
+plt.title("GDP against Mortality Rate")
+plt.xlabel("GDP")
+plt.ylabel("Mortality Rate")
 plt.show()
-
-
